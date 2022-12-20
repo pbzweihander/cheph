f2df0bf20e1c794cfcfeabf547c131fa97ffebf9
pbzweihander <pbzweihander@gmail.com>
2022-12-20 17:04:26 +0900

    Show tag name in tag view

--- frontend/src/PhotosByTag.tsx
+++ frontend/src/PhotosByTag.tsx
@@ -17,16 +17,21 @@ function PhotosByTag() {
   }
 
   return (
-    <div className="grid grid-cols-3 md:grid-cols-6 gap-4 items-center">
-      {metadatas?.map((metadata) => (
-        <PhotoCard key={metadata.name} metadata={metadata} />
-      ))}
-      {isFetching && (
-        <div className="max-w-sm flex justify-center items-center">
-          <Spinner />
-        </div>
-      )}
-      <ObservationComponent />
+    <div>
+      <h2 className="mb-5 inline-block bg-gray-300 rounded-full px-6 py-2 text-2xl font-semibold text-gray-700 break-keep">
+        #{tag}
+      </h2>
+      <div className="grid grid-cols-3 md:grid-cols-6 gap-4 items-center">
+        {metadatas?.map((metadata) => (
+          <PhotoCard key={metadata.name} metadata={metadata} />
+        ))}
+        {isFetching && (
+          <div className="max-w-sm flex justify-center items-center">
+            <Spinner />
+          </div>
+        )}
+        <ObservationComponent />
+      </div>
     </div>
   );
 }
