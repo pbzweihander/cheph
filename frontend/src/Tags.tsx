2feab29282ef86f941ce3fc70a510b1dba70f6e5
pbzweihander <pbzweihander@gmail.com>
2022-12-20 17:06:36 +0900

    Update tags view style

--- frontend/src/Tags.tsx
+++ frontend/src/Tags.tsx
@@ -21,8 +21,8 @@ function Tags() {
       >
         <Link to={`/photos-by-tag/${tag}`}>
           <div className="w-full h-full px-10 py-20 flex justify-center items-center backdrop-blur-sm">
-            <div className="p-1 text-center text-2xl text-[4vw] md:text-[2vw] break-keep font-semibold bg-white rounded-full px-4 py-2">
-              {tag}
+            <div className="p-1 text-center text-2xl text-[4vw] md:text-[2vw] break-keep font-semibold bg-gray-300 rounded-full px-3 py-1 md:px-6 md:py-2 text-gray-700">
+              #{tag}
             </div>
           </div>
         </Link>
